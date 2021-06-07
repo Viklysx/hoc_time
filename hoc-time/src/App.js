c568fc5f02ad335552edb69b3a5cf6f1aec9eb83
Viklysx <vikbelec111@yandex.ru>
2021-06-08 02:08:31 +0300

    feat: changing the work with the date

--- hoc-time/src/App.js
+++ hoc-time/src/App.js
@@ -6,11 +6,11 @@ function App() {
   const [list, setList] = useState([
     {
         url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
-        date: '2017-07-31 13:24:00'
+        date: '2021-06-08 02:00:00'
     },
     {
         url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
-        date: '2018-03-03 12:10:00'
+        date: '2021-06-08 01:00:00'
     },
     {
         url: 'https://www.youtube.com/embed/xGRjCa49C6U?rel=0&amp;controls=0&amp;showinfo=0',
--- hoc-time/src/components/DateTimePretty.js
+++ hoc-time/src/components/DateTimePretty.js
@@ -5,18 +5,17 @@ export const DateTimePretty = (Component) => (props) => {
     const minute = 60 * 1000;
     const day = 24*hour;
     const currentTime = new Date().getTime() - new Date(props.date).getTime();
+    let date;
     
     if (currentTime > day) {
-        return(<Component {...props} date={`${Math.round(currentTime/day)} дней назад`} />);
+        date=`${Math.round(currentTime/day)} дней назад`;
+    } else if (currentTime < hour) {
+        date=`${Math.round(currentTime/minute)} минут назад`;
+    } else if (currentTime >= hour) {
+        date=`${Math.round(currentTime/hour)} часов назад`;
     }
 
-    if (currentTime < hour) {
-        return(<Component {...props} date={`${Math.round(currentTime/minute)} минут назад`} />);
-    } // если прошло меньше часа 
-
-    if (currentTime >= hour) {
-        return(<Component {...props} date={`${Math.round(currentTime/hour)} часов назад`} />);
-    } // если прошло больше часа
+    return(<Component {...props} date={date} />);
 };
 
 export const NewTime = DateTimePretty(DateTime);
